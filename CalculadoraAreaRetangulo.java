4378261e7765e52595ad84be3a8a987ff0e43e58
user <user@users-MacBook-Pro.local>
2025-11-12 15:14:16 -0300

    Calculadora de Area

---
+++ CalculadoraAreaRetangulo.java
@@ -0,0 +1,17 @@
+public class CalculadoraAreaRetangulo {
+  public static void main(String[] args) {
+  	 
+
+  	 //declaração de variáveis
+     double largura = 5.0;
+     double altura = 3.0;
+
+     //calculando a area do retangulo
+     double area = largura * altura;
+     
+     //exibicao resultado
+    System.out.println("Largura do retângulo: " + largura);
+    System.out.println("Altura do retângulo: " + altura);
+    System.out.println("Área do retângulo: " + area);
+  }
+}
